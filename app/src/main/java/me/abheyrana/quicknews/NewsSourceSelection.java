87b2d5e67e8d3a23114b6d4a75d82b722bd49894
Abhey Rana <abheyranacool123.ar@gmail.com>
2017-06-02 19:17:04 +0530

    Added necessary resource files for news source items

---
+++ app/src/main/java/me/abheyrana/quicknews/NewsSourceSelection.java
@@ -0,0 +1,13 @@
+package me.abheyrana.quicknews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsSourceSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_source_selection);
+    }
+}
